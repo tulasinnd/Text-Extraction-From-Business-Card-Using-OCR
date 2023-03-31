5e463e5b3ef5f7cdd95dbd4d0515630785912493
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-03-31 16:25:37 +0530

    Update Text_Extract.py

--- Text_Extract.py
+++ Text_Extract.py
@@ -26,7 +26,7 @@ if image is not None:
     input_image = Image.open(image) #read image
     with col1:
         #st.write("## YOUR IMAGE")
-        st.image(input_image) #display image        
+        # st.image(input_image) #display image        
     
     result = reader.readtext(np.array(input_image))
     result_text = [] #empty list for results
@@ -34,105 +34,105 @@ if image is not None:
         result_text.append(text[1])
         st.write(text[1])
           
-    PH=[]
-    PHID=[]  
-    ADD=set()
-    AID=[]
-    EMAIL=''
-    EID=''
-    PIN=''
-    PID=''
-    WEB=''
-    WID=''
+#     PH=[]
+#     PHID=[]  
+#     ADD=set()
+#     AID=[]
+#     EMAIL=''
+#     EID=''
+#     PIN=''
+#     PID=''
+#     WEB=''
+#     WID=''
     
-    for i, string in enumerate(result_text):   
-        #st.write(string.lower())     
+#     for i, string in enumerate(result_text):   
+#         #st.write(string.lower())     
         
-        # TO FIND EMAIL
-        if re.search(r'@', string.lower()):
-            EMAIL=string.lower()
-            EID=i
+#         # TO FIND EMAIL
+#         if re.search(r'@', string.lower()):
+#             EMAIL=string.lower()
+#             EID=i
         
-        # TO FIND PINCODE
-        match = re.search(r'\d{6,7}', string.lower())
-        if match:
-            PIN=match.group()
-            PID=i
+#         # TO FIND PINCODE
+#         match = re.search(r'\d{6,7}', string.lower())
+#         if match:
+#             PIN=match.group()
+#             PID=i
                        
-        # TO FIND PHONE NUMBER    
-        # match = re.search(r'(?:ph|phone|phno)?(?:[+-]?\d*){7,}', string)
-        #match = re.search(r'(?:ph|phone|phno)?\s*(?:[+-]?\d\s*){7,}', string)
-        match = re.search(r'(?:ph|phone|phno)?\s*(?:[+-]?\d\s*[\(\)]*){7,}', string)
-        if match and len(re.findall(r'\d', string)) > 7:
-            PH.append(string)
-            PHID.append(i)
+#         # TO FIND PHONE NUMBER    
+#         # match = re.search(r'(?:ph|phone|phno)?(?:[+-]?\d*){7,}', string)
+#         #match = re.search(r'(?:ph|phone|phno)?\s*(?:[+-]?\d\s*){7,}', string)
+#         match = re.search(r'(?:ph|phone|phno)?\s*(?:[+-]?\d\s*[\(\)]*){7,}', string)
+#         if match and len(re.findall(r'\d', string)) > 7:
+#             PH.append(string)
+#             PHID.append(i)
 
 
             
-        # TO FIND ADDRESS 
-        keywords = ['road', 'floor', ' st ', 'st,', 'street', ' dt ', 'district',
-                    'near', 'beside', 'opposite', ' at ', ' in ', 'center', 'main road',
-                   'state','country', 'post','zip','city','zone','mandal','town','rural',
-                    'circle','next to','across from','area','building','towers','village',
-                    ' ST ',' VA ',' VA,',' EAST ',' WEST ',' NORTH ',' SOUTH ']
-        # Define the regular expression pattern to match six or seven continuous digits
-        digit_pattern = r'\d{6,7}'
-        # Check if the string contains any of the keywords or a sequence of six or seven digits
-        if any(keyword in string.lower() for keyword in keywords) or re.search(digit_pattern, string):
-            ADD.add(string)
-            AID.append(i)
+#         # TO FIND ADDRESS 
+#         keywords = ['road', 'floor', ' st ', 'st,', 'street', ' dt ', 'district',
+#                     'near', 'beside', 'opposite', ' at ', ' in ', 'center', 'main road',
+#                    'state','country', 'post','zip','city','zone','mandal','town','rural',
+#                     'circle','next to','across from','area','building','towers','village',
+#                     ' ST ',' VA ',' VA,',' EAST ',' WEST ',' NORTH ',' SOUTH ']
+#         # Define the regular expression pattern to match six or seven continuous digits
+#         digit_pattern = r'\d{6,7}'
+#         # Check if the string contains any of the keywords or a sequence of six or seven digits
+#         if any(keyword in string.lower() for keyword in keywords) or re.search(digit_pattern, string):
+#             ADD.add(string)
+#             AID.append(i)
             
-        # TO FIND STATE (USING SIMILARITY SCORE)
-        states = ['Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chhattisgarh', 'Goa', 'Gujarat', 
-          'Haryana','Hyderabad', 'Himachal Pradesh', 'Jharkhand', 'Karnataka', 'Kerala', 'Madhya Pradesh',
-            'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Punjab', 
-            'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal',
-              "United States", "China", "Japan", "Germany", "United Kingdom", "France", "India", 
-               "Canada", "Italy", "South Korea", "Russia", "Australia", "Brazil", "Spain", "Mexico", 'USA','UK']
+#         # TO FIND STATE (USING SIMILARITY SCORE)
+#         states = ['Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chhattisgarh', 'Goa', 'Gujarat', 
+#           'Haryana','Hyderabad', 'Himachal Pradesh', 'Jharkhand', 'Karnataka', 'Kerala', 'Madhya Pradesh',
+#             'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Punjab', 
+#             'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal',
+#               "United States", "China", "Japan", "Germany", "United Kingdom", "France", "India", 
+#                "Canada", "Italy", "South Korea", "Russia", "Australia", "Brazil", "Spain", "Mexico", 'USA','UK']
 
-        import Levenshtein
-        def string_similarity(s1, s2):
-            distance = Levenshtein.distance(s1, s2)
-            similarity = 1 - (distance / max(len(s1), len(s2)))
-            return similarity * 100
+#         import Levenshtein
+#         def string_similarity(s1, s2):
+#             distance = Levenshtein.distance(s1, s2)
+#             similarity = 1 - (distance / max(len(s1), len(s2)))
+#             return similarity * 100
         
-        for x in states:
-            similarity = string_similarity(x.lower(), string.lower())
-            if similarity > 50:
-                ADD.add(string)
-                AID.append(i)
+#         for x in states:
+#             similarity = string_similarity(x.lower(), string.lower())
+#             if similarity > 50:
+#                 ADD.add(string)
+#                 AID.append(i)
                 
-        # WEBSITE URL          
-        if re.match(r"(?!.*@)(www|.*com$)", string):
-            WEB=string.lower()
-            WID=i 
-    with col3: 
-        # DISPLAY ALL THE ELEMENTS OF BUSINESS CARD 
-        st.write("## EXTRACTED TEXT")
-        st.write('##### :red[WEBSITE URL: ] '+ str(WEB))
-        st.write('##### :red[EMAIL: ] '+ str(EMAIL)) 
-        st.write('##### :red[PIN CODE: ] '+ str(PIN)) 
-        ph_str = ', '.join(PH)
-        st.write('##### :red[PHONE NUMBER(S): ] '+ph_str)
-        add_str = ' '.join([str(elem) for elem in ADD])
-        st.write('##### :red[ADDRESS: ] ', add_str)
+#         # WEBSITE URL          
+#         if re.match(r"(?!.*@)(www|.*com$)", string):
+#             WEB=string.lower()
+#             WID=i 
+#     with col3: 
+#         # DISPLAY ALL THE ELEMENTS OF BUSINESS CARD 
+#         st.write("## EXTRACTED TEXT")
+#         st.write('##### :red[WEBSITE URL: ] '+ str(WEB))
+#         st.write('##### :red[EMAIL: ] '+ str(EMAIL)) 
+#         st.write('##### :red[PIN CODE: ] '+ str(PIN)) 
+#         ph_str = ', '.join(PH)
+#         st.write('##### :red[PHONE NUMBER(S): ] '+ph_str)
+#         add_str = ' '.join([str(elem) for elem in ADD])
+#         st.write('##### :red[ADDRESS: ] ', add_str)
 
-        IDS= [EID,PID,WID]
-        IDS.extend(AID)
-        IDS.extend(PHID)
-#         st.write(IDS)
-        oth=''                               
-        fin=[]                        
-        for i, string in enumerate(result_text):
-            if i not in IDS:
-                if len(string) >= 4 and ',' not in string and '.' not in string and 'www.' not in string:
-                    if not re.match("^[0-9]{0,3}$", string) and not re.match("^[^a-zA-Z0-9]+$", string):
-                        numbers = re.findall('\d+', string)
-                        if len(numbers) == 0 or all(len(num) < 3 for num in numbers) and not any(num in string for num in ['0','1','2','3','4','5','6','7','8','9']*3):
-                            fin.append(string)
-        st.write('##### :red[CARD HOLDER & COMPANY DETAILS: ] ')
-        for i in fin:
-            st.write('##### '+i)
+#         IDS= [EID,PID,WID]
+#         IDS.extend(AID)
+#         IDS.extend(PHID)
+# #         st.write(IDS)
+#         oth=''                               
+#         fin=[]                        
+#         for i, string in enumerate(result_text):
+#             if i not in IDS:
+#                 if len(string) >= 4 and ',' not in string and '.' not in string and 'www.' not in string:
+#                     if not re.match("^[0-9]{0,3}$", string) and not re.match("^[^a-zA-Z0-9]+$", string):
+#                         numbers = re.findall('\d+', string)
+#                         if len(numbers) == 0 or all(len(num) < 3 for num in numbers) and not any(num in string for num in ['0','1','2','3','4','5','6','7','8','9']*3):
+#                             fin.append(string)
+#         st.write('##### :red[CARD HOLDER & COMPANY DETAILS: ] ')
+#         for i in fin:
+#             st.write('##### '+i)
             
-#         st.write(result_text)
-#         st.write(PH)
+# #         st.write(result_text)
+# #         st.write(PH)
