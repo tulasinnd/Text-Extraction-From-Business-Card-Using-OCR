1346be090f514c6208719b67089850def9fda48a
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-03-31 15:15:52 +0530

    Create Text_Extract.py

---
+++ Text_Extract.py
@@ -0,0 +1,41 @@
+import easyocr as ocr  #OCR
+import streamlit as st  #Web App
+from PIL import Image #Image Processing
+import numpy as np #Image Processing 
+st. set_page_config(layout="wide")
+import re
+import pandas as pd
+
+#title
+st.title(":orange[UNLOCKING DATA]") 
+st.write(" ")
+col1, col2,col3= st.columns([3,0.5,4.5])
+with col1:
+    #image uploader
+    st.write("## UPLOAD IMAGE")
+    image = st.file_uploader(label = "",type=['png','jpg','jpeg'])
+
+@st.cache
+def load_model(): 
+    reader = ocr.Reader(['en'])#,model_storage_directory='.')
+    return reader 
+
+reader = load_model() #load model
+
+if image is not None:
+    input_image = Image.open(image) #read image
+    with col1:
+        #st.write("## YOUR IMAGE")
+        st.image(input_image) #display image        
+    
+    result = reader.readtext(np.array(input_image))
+    result_text = [] #empty list for results
+    for text in result:
+        result_text.append(text[1])
+    with col3:
+      for t in result_text:
+        print(t)
+
+      
+          
+    
